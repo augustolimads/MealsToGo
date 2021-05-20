be07b785d9991304beba959d5ccdc89de271e860
Augusto Lima <augustolimads@gmail.com>
2021-05-20 11:32:51 -0300

    fix part of api error

---
+++ src/features/restaurants/screens/Restaurant.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const SearchContainer = styled.View`
+  background-color: white;
+  padding: 16px;
+`;
+
+export const RestaurantList = styled.FlatList.attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
